d3b489436f4a0316fdddbb46b8b3392f12d0914a
StefanAdrian <adymartianul@gmail.com>
2016-06-13 03:38:19 +0300

    .

--- Caesar Cipher/Form1.Designer.cs
+++ Caesar Cipher/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mangal", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mangal", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Mangal", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(89, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Mangal", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(184, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mangal", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(89, 31);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(300, 20);
+            this.textBox.TabIndex = 5;
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Location = new System.Drawing.Point(89, 70);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(100, 20);
+            this.keyTextBox.TabIndex = 6;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(89, 153);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(300, 20);
+            this.resultTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "1 to 25";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(424, 222);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Caesar\'s Cipher";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Caesar Cipher/Form1.cs
+++ Caesar Cipher/Form1.cs
@@ -16,5 +16,66 @@ namespace Caesar_Cipher
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char temp;
+            char[] encryptedText = textBox.Text.ToCharArray();
+            for (int i = 0; i < encryptedText.Length; i++)
+            {
+                if (encryptedText[i] == ' ')
+                {
+                    continue;
+                }
+                else
+                {
+                    temp = (char)(encryptedText[i] + Convert.ToInt32(keyTextBox.Text));
+                    if (temp > 'z')
+                    {
+                        temp = (char)(temp - 26);
+                    }
+                    else if (temp < 'a')
+                    {
+                        temp = (char)(temp + 26);
+                    }
+                    encryptedText[i] = temp;
+                }
+            }
+            string result = new string(encryptedText);
+            resultTextBox.Text = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char temp;
+            char[] decryptedText = textBox.Text.ToCharArray();
+            for (int i = 0; i < decryptedText.Length; i++)
+            {
+                if (decryptedText[i] == ' ')
+                {
+                    continue;
+                }
+                else
+                {
+                    temp = (char)(decryptedText[i] - Convert.ToInt32(keyTextBox.Text));
+                    if (temp > 'z')
+                    {
+                        temp = (char)(temp - 26);
+                    }
+                    else if (temp < 'a')
+                    {
+                        temp = (char)(temp + 26);
+                    }
+                    decryptedText[i] = temp;
+                }
+            }
+            string result = new string(decryptedText);
+            resultTextBox.Text = result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
